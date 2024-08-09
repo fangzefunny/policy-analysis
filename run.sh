c0eb191583a80e7f04b435afa0f5c92ef10f0cc1
Zeming <fangzefunny@gmail.com>
2024-08-09 16:13:23 +0800

    clear up some script

--- run.sh
+++ run.sh
@@ -10,11 +10,12 @@ python m0_preprocess.py
 ## STEP 2: Fit models to the data and estimate the parameters 
 declare models=("MOS6" "MOS22" "FLR6" "FLR22" "RS3" "RS13" "PH4" "PH17" 
                 "EU_MO" "EU_HA" "MO_HA" "PS_MO_HA" "EU_RD_HA" 
-                "EU_PS_MO_HA" "EU_MO_HA_RD" "linear_comb")
+                "EU_PS_MO_HA" "EU_MO_HA_RD"
+                "EU_MO18" "EU_HA18" "MO_HA18" "PS_MO_HA22" "EU_PS_MO_HA26")
 for data_set in "${data_sets[@]}"; do 
     for model in "${models[@]}"; do 
         echo Data set=$data_set  Model=$model Method=$method
-        python m1_fit.py -d=$data_set -n=$model -s=420 -f=30 -c=30 -m=$method -a=$alg
+        python m1_fit.py -d=$data_set -n=$model -s=420 -f=40 -c=40 -m=$method -a=$alg
         python m2_rollout.py -d=$data_set -n=$model -f=20 -c=20 -m=$method -a=$alg
     done  
 done
